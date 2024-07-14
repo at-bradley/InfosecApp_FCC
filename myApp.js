a180f8b41f94787d68b3ed0248fb273a24377165
Anthony Thudium <ab.thudium@gmail.com>
2024-07-14 00:57:21 -0700

    Set content security policy

--- myApp.js
+++ myApp.js
@@ -15,7 +15,11 @@ app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({maxAge:ninetyDaysInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
-
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+  defaultSrc: ["'self'"],
+  scriptSrc: ["'self'", 'trusted-cdn.com']
+}}));
 // your code
 
 
